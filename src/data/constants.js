bdfc5654ff332968abae5dd5b5c3768fbcb6acc3
ishimwethierryhenry <ishimweth@gmail.com>
2025-08-14 11:49:02 +0200

    awesomity5

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,7 @@
 // export const variables = { SERVER_URL: "https://pool-mgt-be.onrender.com" };
 
 
-export const variables = { SERVER_URL: "http://localhost:3000" };
+// export const variables = { SERVER_URL: "http://localhost:3000" };
 
 // export const variables = {
 //   SERVER_URL: process.env.NODE_ENV === 'production' 
@@ -12,3 +12,6 @@ export const variables = { SERVER_URL: "http://localhost:3000" };
 // export const variables = { 
 //   SERVER_URL: "https://swift-backend-lake.vercel.app" 
 // };
+
+
+export const variables = { SERVER_URL: "https://swift-backend-88o8.onrender.com" };
